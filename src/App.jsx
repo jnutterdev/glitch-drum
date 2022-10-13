b031249c2be20b86fa7f69a4b43b757c1fcb244f
jnutterdev <hello@jnutterdev.com>
2022-10-12 22:19:19 -0400

    started on drum machine layout, added in some tailwind styles. still need to build out components, logic, modules, etc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,108 +1,18 @@
-import { useState } from "react";
-import "./App.css";
+
+import Drum from "./components/partials/Drum";
+import DrumMachine from "./components/DrumMachine";
 
 function App() {
-  const [count, setCount] = useState(0);
+ 
 
   return (
     <div className="App">
-      <div class="space-controls">
-        <h1>Drum machine</h1>
-        <h4>Kick</h4>
-        <div class="pattern" id="kick">
-          <input type="checkbox" class="alien" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-        <h4>Hihat</h4>
-        <div class="pattern" id="hihat">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-        <br />
-        <h4>Snare</h4>
-        <div class="pattern" id="snare">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-        <br />
-        <h4>Crash</h4>
-        <div class="pattern" id="crash">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-        <br />
-        <h4>Clap</h4>
-        <div class="pattern" id="clap">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-        <br />
-        <h4>Tom</h4>
-        <div class="pattern" id="tom">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-        <br />
-        <h4>Cbell</h4>
-        <div class="pattern" id="cowbell">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-        <br />
-        <h4>Zap</h4>
-        <div class="pattern" id="zap">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-      </div>
+
+        <DrumMachine />
+        <Drum />
+        <Drum />
+        <Drum />
+        <Drum />
     </div>
   );
 }
---
+++ src/components/partials/Drum.jsx
@@ -0,0 +1,19 @@
+function Drum() {
+
+    return (
+        <>
+        <div className="pattern" id="kick">
+            <input type="checkbox" className="h-8 w-8 rounded-full shadow mx-3 bg-red-100 border-red-300 text-red-500 focus:ring-red-200" />
+            <input type="checkbox" className="h-8 w-8 rounded-full shadow mx-3 bg-red-100 border-red-300 text-red-500 focus:ring-red-200" />
+            <input type="checkbox" className="h-8 w-8 rounded-full shadow mx-3 bg-red-100 border-red-300 text-red-500 focus:ring-red-200"/>
+            <input type="checkbox" className="h-8 w-8 rounded-full shadow mx-3 bg-red-100 border-red-300 text-red-500 focus:ring-red-200"/>
+            <input type="checkbox" className="h-8 w-8 rounded-full shadow mx-3 bg-red-100 border-red-300 text-red-500 focus:ring-red-200" />
+            <input type="checkbox" className="h-8 w-8 rounded-full shadow mx-3 bg-red-100 border-red-300 text-red-500 focus:ring-red-200" />
+            <input type="checkbox" className="h-8 w-8 rounded-full shadow mx-3 bg-red-100 border-red-300 text-red-500 focus:ring-red-200"/>
+            <input type="checkbox" className="h-8 w-8 rounded-full shadow mx-3 bg-red-100 border-red-300 text-red-500 focus:ring-red-200" />
+        </div>
+        </>
+    )
+}
+
+export default Drum 
